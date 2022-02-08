4cf7c80cf38dabf17b05ea26a85a7c00ce6fc32c
Feride Metin <feridemetin97@gmail.com>
2022-02-08 15:31:38 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+
+function App() {
+  const [loading, setLoading] = useState(null);
+  const [coords, setCoords] = useState(null);
+  const [current, setCurrent] = useState({
+    temp_c: '',
+    icon: '',
+    feelslike_c: ''
+  });
+
+  const [location, setLocation] = useState({
+    name: '',
+    country: '',
+    region: '',
+    localtime: ''
+  });
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      alert('Geolocation yok');
+    }
+    else {
+      setLoading(true);
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCoords(`${position.coords.latitude}, ${position.coords.longitude}`);
+      });
+    }
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    const apiURL = `http://api.weatherapi.com/v1/current.json?key=9bf44c24d42a410581b112909220802&q=${coords}&aqi=no`
+    fetch(apiURL).then(res => res.json()).then((data) => {
+      setCurrent({
+        temp: data.current.temp_c,
+        icon: data.current.condition.icon,
+        feelslike_c: data.current.feelslike_c
+      });
+      setLocation({
+        name: data.location.name,
+        region: data.location.region,
+        country: data.location.country,
+        localtime: data.location.localtime
+      });
+      setLoading(false);
+    })
+  }, [coords]);
+
+  return (
+    <div className="App">
+      <button className='button' onClick={getLocation}>Hava Durumunu Getir</button>
+      {!loading && 
+       <div>
+       <div>
+         <div className='weather'>
+           <img src={current.icon} alt="Weather" />
+           <div>
+             Sıcaklık
+             <span>{current.temp}</span>
+           </div>
+           <div>
+             Hissedilen
+             <span>{current.feelslike_c}</span>
+           </div>
+         </div>
+       </div>
+
+       <div>
+         <div className='location'>
+           <span>{location.name}</span> - <span>{location.region}</span> - <span>{location.country}</span>
+         </div>
+         <div><span className='time'>{location.localtime}</span></div>
+       </div>
+     </div>
+      }
+
+      {loading && <div className='loading'><span>Yükleniyor..</span></div>}
+
+    </div>
+
+  );
+}
+
+export default App;
